5f03b72c20869a17125befe2b1c0bb85e73f03e2
Kresna Satya <satyakresna@unud.ac.id>
2024-07-03 11:46:58 +0800

    redesign home and navigation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,48 @@
 export default {
-    title: 'RistekUSDI',
-    description: 'Dokumentasi pustaka karya Ristek USDI.',
+    title: 'Ristek USDI',
+    description: 'Riset Teknologi Unit Sumber Daya Informasi. Berdikari dan Tangguh!',
     themeConfig: {
         siteTitle: 'Ristek USDI',
         nav: [
-            { text: 'SSO PHP', link: '/sso-php' },
-            { text: 'SSO Laravel', link: '/sso-laravel' },
+            { text: 'Panduan', link: '/guide/introduction' },
+            { text: 'Perkakas', link: '/tools/introduction' },
+        ],
+        sidebar: {
+            '/guide/': { base: '/guide/', items: sidebarGuide() },
+            '/tools/': { base: '/tools/', items: sidebarTools() },
+        },
+        search: {
+            provider: 'local'
+        },
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/ristekusdi' }
         ]
     },
     // deployment
     base: '/'
-}  
+}
+
+function sidebarGuide() {
+    return [
+        {
+            text: 'Panduan',
+            items: [
+                { text: 'Pengantar', link: 'introduction' },
+                { text: 'Membuat Pustaka', link: 'create-package' },
+            ]
+        }
+    ]
+}
+
+function sidebarTools() {
+    return [
+        {
+            text: 'Perkakas',
+            items: [
+                { text: 'Pengantar', link: 'introduction' },
+                { text: 'SSO PHP', link: 'sso-php' },
+                { text: 'SSO Laravel', link: 'sso-laravel' },
+            ]
+        }
+    ]
+}
--- docs/index.md
+++ docs/index.md
@@ -1,10 +1,18 @@
-# Ristek USDI
+---
+layout: home
 
-> Berdikari dan tangguh!
-
-Selamat datang di situs Ristek USDI. Situs ini memuat dokumentasi pustaka-pustaka karya Ristek USDI antara lain:
-
-- [sso-php](/sso-php)
-- [sso-laravel](/sso-laravel)
-
-> Catatan: Khusus untuk package SSO, jika Anda menggunakan JavaScript maka kami sarankan Anda menggunakan pustaka bawaan Keycloak yakni [Keycloak JS](https://www.npmjs.com/package/keycloak-js).
+hero:
+    name: Ristek USDI
+    text: Riset Teknologi Unit Sumber Daya Informasi Universitas Udayana
+    tagline: Berdikari dan tangguh!
+    actions:
+        - theme: brand
+          text: Panduan
+          link: /guide/introduction
+        - theme: alt
+          text: Perkakas
+          link: /tools/introduction
+        - theme: alt
+          text: GitHub 
+          link: https://github.com/ristekusdi
+---
